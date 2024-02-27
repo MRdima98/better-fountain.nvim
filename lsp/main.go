d2137a745dcb675c3a1df3bebfa4ff315c920055
dima <dimafrunza69@gmail.com>
2024-02-27 10:13:14 +0100

    Added logs, basic text completion and basic highlight

--- lsp/main.go
+++ lsp/main.go
@@ -9,17 +9,20 @@ import (
 	_ "github.com/tliron/commonlog/simple"
 )
 
-const lsName = "Emoji Autocomplete Language Server"
+const lsName = "gotem"
+var fileLog = "/tmp/lsp.log"
 
 var version string = "0.0.1"
 var handler protocol.Handler
 
 func main() {
-	commonlog.Configure(2, nil)
+	commonlog.Configure(2, &fileLog)
 
-	handler = protocol.Handler{
+	handler = protocol.Handler {
 		Initialize:             initialize,
 		Shutdown:               shutdown,
+        TextDocumentColor: textDocumentColor,
+        TextDocumentCompletion: textDocumentCompletion,
 	}
 
 	server := server.NewServer(&handler, lsName, true)
@@ -47,3 +50,26 @@ func shutdown(context *glsp.Context) error {
 	return nil
 }
 
+func textDocumentColor(context *glsp.Context, params *protocol.DocumentColorParams) ([]protocol.ColorInformation, error) {
+    colors := make([]protocol.ColorInformation, 1)
+    color := protocol.ColorInformation {
+        Range: protocol.Range { 
+            Start: protocol.Position{ Line: 2, Character: 3 },
+            End: protocol.Position{ Line: 2, Character: 10 },
+        },
+        Color: protocol.Color { Red: 1.0, Green: 0.0, Blue: 0.0, Alpha: 0.0 },
+    }
+    colors[0] = color
+    return colors, nil
+}
+func textDocumentCompletion(context *glsp.Context, params *protocol.CompletionParams) (any, error) {
+    hello := "Hello"
+    return protocol.CompletionList {
+        IsIncomplete: true,
+        Items: []protocol.CompletionItem {
+            {
+                InsertText: &hello,
+            },
+        },
+    }, nil
+}
