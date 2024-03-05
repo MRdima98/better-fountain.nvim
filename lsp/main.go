338a80a6ea340aa17ae3cb22a929820076aba672
dima <dimafrunza69@gmail.com>
2024-03-05 07:17:15 +0100

    First successful token highlight

--- lsp/main.go
+++ lsp/main.go
@@ -22,7 +22,14 @@ func main() {
 	handler = protocol.Handler{
 		Initialize:             initialize,
 		Shutdown:               shutdown,
-		TextDocumentColor:      textDocumentColor,
+        LogTrace: logTrace,
+        TextDocumentSemanticTokensFull: tokens,
+        TextDocumentDidOpen: didOpen,
+        TextDocumentDidChange: didChange,
+        TextDocumentDidClose: didClose,
+        TextDocumentColor: color,
+        TextDocumentDocumentHighlight: highlights,
+        TextDocumentDocumentSymbol: documentSymbol,
 		TextDocumentCompletion: textDocumentCompletion,
 	}
 
@@ -36,7 +43,23 @@ func initialize(context *glsp.Context, params *protocol.InitializeParams) (any, 
 
 	capabilities := handler.CreateServerCapabilities()
 
-	capabilities.CompletionProvider = &protocol.CompletionOptions{}
+
+    capabilities.CompletionProvider = &protocol.CompletionOptions{}
+    capabilities.DocumentHighlightProvider = &protocol.DocumentHighlightOptions{ }
+
+    capabilities.DocumentSymbolProvider = &protocol.DocumentSymbolOptions{ }
+
+    capabilities.SemanticTokensProvider = &protocol.SemanticTokensOptions{
+        Legend: protocol.SemanticTokensLegend{
+            TokenTypes: []string{"type"},
+
+            TokenModifiers: []string{"private"},
+        },
+        Full: true,
+    }
+
+    capabilities.ColorProvider = &protocol.DocumentColorOptions{}
+
 
 	return protocol.InitializeResult{
 		Capabilities: capabilities,
@@ -51,19 +74,6 @@ func shutdown(context *glsp.Context) error {
 	return nil
 }
 
-func textDocumentColor(context *glsp.Context, params *protocol.DocumentColorParams) ([]protocol.ColorInformation, error) {
-	colors := make([]protocol.ColorInformation, 1)
-	color := protocol.ColorInformation{
-		Range: protocol.Range{
-			Start: protocol.Position{Line: 2, Character: 3},
-			End:   protocol.Position{Line: 2, Character: 10},
-		},
-		Color: protocol.Color{Red: 1.0, Green: 0.0, Blue: 0.0, Alpha: 0.0},
-	}
-	colors[0] = color
-	return colors, nil
-}
-
 func textDocumentCompletion(context *glsp.Context, params *protocol.CompletionParams) (any, error) {
 	hello := "wakanda"
 
@@ -74,4 +84,82 @@ func textDocumentCompletion(context *glsp.Context, params *protocol.CompletionPa
             Detail: &hello,
         },
     }, nil
+}
+
+func documentSymbol(context *glsp.Context, params *protocol.DocumentSymbolParams) (any, error) {
+	commonlog.NewInfoMessage(0, "Symbols")
+    // var symbols []protocol.DocumentSymbol
+
+    return []protocol.DocumentSymbol{
+        {
+            Name: "symbols",
+            Range: protocol.Range{
+                Start: protocol.Position{ 
+                    Line: 0,
+                    Character: 0,
+                },
+                End: protocol.Position{ 
+                    Line: 0,
+                    Character: 5,
+                },
+            },
+            Kind: 6,
+        },
+    }, nil
+}
+
+func highlights (context *glsp.Context, params *protocol.DocumentHighlightParams) ([]protocol.DocumentHighlight, error) {
+    var hi []protocol.DocumentHighlight
+    hello := protocol.DocumentHighlightKindWrite
+    hi = append(hi, protocol.DocumentHighlight{
+		Range: protocol.Range{
+			Start: protocol.Position{Line: 0, Character: 0},
+			End:   protocol.Position{Line: 0, Character: 10},
+		},
+        Kind: &hello,
+
+    })
+    return hi, nil
+}
+
+
+func didOpen (context *glsp.Context, params *protocol.DidOpenTextDocumentParams) error {
+    return nil
+}
+
+func didChange (context *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
+    return nil
+}
+func didClose (context *glsp.Context, params *protocol.DidCloseTextDocumentParams) error {
+
+    return nil
+}
+
+func color (context *glsp.Context, params *protocol.DocumentColorParams) ([]protocol.ColorInformation, error) {
+    return []protocol.ColorInformation{
+        {
+            Range: protocol.Range{
+                Start: protocol.Position{Line: 0, Character: 0},
+                End:   protocol.Position{Line: 0, Character: 10},
+            },
+            Color: protocol.Color{
+                Red: 1.0,
+                Green: 0,
+                Blue: 0,
+                Alpha: 0,
+            },
+        },
+    }, nil
+}
+
+func logTrace (context *glsp.Context, params *protocol.LogTraceParams) error {
+	commonlog.NewInfoMessage(4, *params.Verbose)
+    return nil
+}
+
+func tokens (context *glsp.Context, params *protocol.SemanticTokensParams) (*protocol.SemanticTokens, error) {
+    tokens := [] uint32{0,0,4,0,0}
+    return &protocol.SemanticTokens{
+        Data: tokens,
+    },nil
 }
